d46fcfb08582a82f3f71abfb13c12694e5071fd8
Tudzy <narongsak5az@gamil.com>
2023-04-04 23:33:48 +0700

    add token register login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import {registerController} from '../controllers/authController.js'
-
+import { loginController } from '../controllers/authController.js'
 // router object
 const router = express.Router()
 
 // routing
 // Register || Method Post
 router.post('/register', registerController)
+
+// Login || Post
+router.post('/login', loginController)
 
 export default router
