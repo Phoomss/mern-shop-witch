7023114800c863cbb42a861d1c78d582f9956685
Tudzy <narongsak5az@gamil.com>
2023-04-04 20:23:13 +0700

    set authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,13 @@
-export const registerController = () =>{};
+import useModel from "../models/useModel.js";
+
+export const registerController = (req, res) =>{
+    try {
+        const {name,email,password,phone,address} = req.body
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            message: 'Error in Register'
+        })
+    }
+};
