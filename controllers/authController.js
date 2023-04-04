4cc09be6b0da2cd600f4f0002ba9641d153d0a86
Tudzy <narongsak5az@gamil.com>
2023-04-04 20:48:12 +0700

    set register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,51 @@
+import { hashPassword } from "../helpers/authHelper.js";
 import useModel from "../models/useModel.js";
 
-export const registerController = (req, res) =>{
-    try {
-        const {name,email,password,phone,address} = req.body
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success:false,
-            message: 'Error in Register'
-        })
+export const registerController = async (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+
+    // validations
+    if (!name) {
+      return res.send({ error: "Name is Required" });
     }
+    if (!email) {
+      return res.send({ error: "Email is Required" });
+    }
+    if (!password) {
+      return res.send({ error: "Password is Required" });
+    }
+    if (!phone) {
+      return res.send({ error: "Phone is Required" });
+    }
+    if (!address) {
+      return res.send({ error: "Address is Required" });
+    }
+
+    // check user
+    const exisitingUser = await useModel.findOne({ email });
+    // exisiting user
+    if (exisitingUser) {
+      return res.status(200).send({
+        success: true,
+        message: "Already Register please login",
+      });
+    }
+    // register user
+    const hashedPassword = await hashPassword(password)
+    // save
+    const user = new useModel({name,email,phone,address,password:hashedPassword}).save()
+
+    res.status(201).send({
+        success:true,
+        message: "User Register Successfully",
+        user,
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in Register",
+    });
+  }
 };
--- models/useModel.js
+++ models/useModel.js
@@ -32,4 +32,4 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("users");
+export default mongoose.model("users", userSchema);
